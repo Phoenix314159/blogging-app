9bbe3863a3c89cb45f522644e3f9a6c7c2004bd5
James Langford <derivative7117@live.com>
2017-09-16 17:47:26 -0600

    initial commit

---
+++ routes/sampleRoute.js
@@ -0,0 +1,11 @@
+const config = require('../config/config');
+
+module.exports = app => {
+
+    app.get('/api/getinfo', async (req, res) => {
+
+        let response = await axios.get(config.apiUrl);
+
+        res.status(200).send(response);
+    })
+}
