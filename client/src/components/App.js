aa9414c7b688cffaa9eb03ad48eb94be91de4b16
James Langford <derivative7117@live.com>
2017-09-19 21:40:43 -0600

    added css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import '../styles/css/App.css';
 import React, {Component} from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import PostsIndex from '../containers/PostsIndex';
 import PostsNew from '../containers/PostsNew';
 
@@ -10,8 +10,10 @@ class App extends Component {
         return (
             <BrowserRouter>
                 <div>
-                    <Route path="/" component={PostsIndex}/>
-                    <Route path="/posts/new" component={PostsNew}/>
+                    <Switch>
+                        <Route path="/posts/new" component={PostsNew}/>
+                        <Route path="/" component={PostsIndex}/>
+                    </Switch>
                 </div>
             </BrowserRouter>
         );
