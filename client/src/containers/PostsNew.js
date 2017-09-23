870e4a22aa65e0a236d02ef0174bf20964fbbdba
James Langford <derivative7117@live.com>
2017-09-23 13:59:47 -0600

    updated postsNew component

--- client/src/containers/PostsNew.js
+++ client/src/containers/PostsNew.js
@@ -11,21 +11,27 @@ class PostsNew extends Component {
                     type="text"
                     {...field.input}
                 />
+                {field.meta.error}
             </div>
         )
     }
 
+    onSubmit(values) {
+        console.log(values)
+    }
+
     render() {
+        const {handleSubmit} = this.props;
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field
-                    label="Title"
+                    label="Title For Post"
                     name="title"
                     component={this.renderField}
                 />
                 <Field
-                    label="Tags"
-                    name="tags"
+                    label="Categories"
+                    name="categories"
                     component={this.renderField}
                 />
                 <Field
@@ -33,12 +39,27 @@ class PostsNew extends Component {
                     name="content"
                     component={this.renderField}
                 />
-
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         )
     }
 }
 
+const validate = values => {
+    const errors = {};
+    if (!values.title || values.title.length < 3) {
+        errors.title = 'Enter a title that is at least 3 characters!';
+    }
+    if (!values.categories) {
+        errors.categories = 'Enter some categories!';
+    }
+    if (!values.content) {
+        errors.content = 'Enter some content please!';
+    }
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
