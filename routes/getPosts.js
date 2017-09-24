4a45958a86492e7b490b3e6eb58678e922532614
James Langford <derivative7117@live.com>
2017-09-24 12:36:13 -0600

    updated PostsShow component

--- routes/getPosts.js
+++ routes/getPosts.js
@@ -2,9 +2,9 @@ const config = require('../config/config'),
     axios = require('axios');
 
 module.exports = app => {
-
     app.get('/api/getposts', async (req, res) => {
         let response = await axios.get(`${config.url}?${config.api_key}`);
+        console.log(response.data);
         res.status(200).send(response.data);
     })
 }
